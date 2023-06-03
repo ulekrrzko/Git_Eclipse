b98ba0ad4298521dba5def9f15e136b61fb2db87
ulekrrzko <127007897+ulekrrzko@users.noreply.github.com>
2023-06-03 13:52:12 -0600

    1er commit

---
+++ src/paquete1/Ejemplo1.java
@@ -0,0 +1,15 @@
+package paquete1;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Este comentario
+		 * es multilinea
+		 */
+		/**Este comentario es de documentación*/
+		System.out.println("Hola Mundo!!!");
+
+	}
+
+}
