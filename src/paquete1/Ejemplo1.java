2b7a3a6cb3b264e5df4e321ec2073717c47c48ef
ulekrrzko <ucarrascof@gmail.com>
2023-06-05 22:48:54 -0600

    Agregando variables y imprimiendo variable

--- src/paquete1/Ejemplo1.java
+++ src/paquete1/Ejemplo1.java
@@ -10,11 +10,12 @@ public class Ejemplo1 {
 		/**Este comentario es de documentación*/
 		System.out.println("Hola Mundo!!!");
 		System.out.println("Este es un ejercicio con java y git");
-//<<<<<<< HEAD
 		System.out.println("Exito");
-//=======
 		System.out.println("Estamos en rama1_ejemplo1");
-//>>>>>>> rama1_ejemplo1
+		
+		String nombre = "Ulises Carrasco";
+		
+		System.out.println(nombre);
 	} 
 
 }
